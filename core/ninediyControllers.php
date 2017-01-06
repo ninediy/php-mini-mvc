a4f69336c1cf0e5b6609cf3145c3e163cabafe89
ninediy <ninediy.dev@gmail.com>
2017-01-06 16:29:55 +0700

    new function
    add new function : native_curl

--- core/ninediyControllers.php
+++ core/ninediyControllers.php
@@ -31,16 +31,16 @@ class ninediyControllers implements SyntaxFramework {
         $verb = $_SERVER['REQUEST_METHOD'];
         switch ($verb) {
             case 'GET':
-            return $_GET;
-            break;
+                return $_GET;
+                break;
             case 'POST':
             case 'PUT':
-            return json_decode(file_get_contents('php://input'), 1);
-            break;
+                return json_decode(file_get_contents('php://input'), 1);
+                break;
             case 'DELETE':
             default:
-            $return = array();
-            break;
+                $return = array();
+                break;
         }
     }
 
@@ -85,11 +85,42 @@ class ninediyControllers implements SyntaxFramework {
         return json_decode(file_get_contents('php://input'));
     }
 
-    function clean_input($data){
+    function clean_input($data) {
         $data = trim($data);
         $data = stripslashes($data);
         $data = htmlspecialchars($data);
         return $data;
+    }
+
+    function native_curl($new_name, $new_email) {
+        $username = 'admin';
+        $password = '1234';
+
+        // Alternative JSON version
+        // $url = 'http://twitter.com/statuses/update.json';
+        // Set up and execute the curl process
+        $curl_handle = curl_init();
+        curl_setopt($curl_handle, CURLOPT_URL, 'http://localhost/restserver/index.php/example_api/user/id/1/format/json');
+        curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl_handle, CURLOPT_POST, 1);
+        curl_setopt($curl_handle, CURLOPT_POSTFIELDS, array(
+            'name' => $new_name,
+            'email' => $new_email
+        ));
+
+        // Optional, delete this line if your API is open
+        curl_setopt($curl_handle, CURLOPT_USERPWD, $username . ':' . $password);
+
+        $buffer = curl_exec($curl_handle);
+        curl_close($curl_handle);
+
+        $result = json_decode($buffer);
+
+        if (isset($result->status) && $result->status == 'success') {
+            echo 'User has been updated.';
+        } else {
+            echo 'Something has gone wrong';
+        }
     }
 
 }
